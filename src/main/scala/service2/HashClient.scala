2d81989b6064da7900c6ef59bfa0c10f6e2a437c
YohanEngineer <bouali.yohan@yahoo.com>
2022-11-22 09:12:49 +0100

    Setup hash method

--- src/main/scala/service2/HashClient.scala
+++ src/main/scala/service2/HashClient.scala
@@ -1,8 +1,8 @@
 package service2
 
-import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.functions.{col, udf, when}
 import org.apache.spark.sql.types.StringType
+import org.apache.spark.sql.{DataFrame, SparkSession}
 import utils.CSV
 
 
@@ -21,33 +21,36 @@ object HashClient {
 
   def hash(sparkSession: SparkSession, id: Long): Boolean = {
     println("Trying to hash data with id : " + id)
-    val encryptUDF = udf(sha256Hash _)
     val path = "data"
-    val data = CSV.read(sparkSession, path)
+    var data = CSV.read(sparkSession, path)
     val searchedID = data("IdentifiantClient") === id
-    val result = data.filter(searchedID)
+    var result = data.filter(searchedID)
     if (result.count() == 0) {
       return false
     }
-    data.withColumn("Nom", when(col("IdentifiantClient").equalTo(id), encryptUDF(col("Nom").cast(StringType)))
-      .otherwise(col("Nom")
-      )
-    )
-    data.withColumn("Prenom", when(col("IdentifiantClient").equalTo(id), encryptUDF(col("Prenom").cast(StringType)))
-      .otherwise(col("Prenom")
-      )
-    )
-    data.withColumn("Adresse", when(col("IdentifiantClient").equalTo(id), encryptUDF(col("Adresse").cast(StringType)))
-      .otherwise(col("Adresse")
-      )
-    )
-    data.withColumn("DateDeSouscription", when(col("IdentifiantClient").equalTo(id), encryptUDF(col("DateDeSouscription").cast(StringType)))
-      .otherwise(col("DateDeSouscription")
-      )
-    )
-    CSV.write("data", data)
-    return true
+    data = hashColumn(data,"IdentifiantClient","Nom", id)
+    data = hashColumn(data,"IdentifiantClient","Prenom", id)
+    data = hashColumn(data,"IdentifiantClient","Adresse", id)
+    data = hashColumn(data,"IdentifiantClient","DateDeSouscription", id)
+    result = data.filter(searchedID)
+    result.show()
+    CSV.write("temp", data)
+    true
   }
 
+  def hashColumn(data: DataFrame, identifyingColumn: String,columnName: String, id: Long): DataFrame = {
+    val encryptUDF = udf(sha256Hash _)
+    data.withColumn(columnName, when(col(identifyingColumn).equalTo(id), encryptUDF(col(columnName).cast(StringType)))
+      .otherwise(col(columnName)
+      )
+    )
+  }
+
+//  def replaceFile(sc: SparkSession) = {
+//    import org.apache.hadoop.fs._
+//    val conf = sc.sparkContext.hadoopConfiguration
+//    val fs = FileSystem.get(conf)
+//    fs.rename(new Path("temp/part-0000*"), new Path("data/store.csv"))
+//  }
 
 }
