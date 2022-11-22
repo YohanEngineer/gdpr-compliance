8d843612800a134e44c4db8e88a5352fc118be46
Selim <selim.bouhassatine@etu.u-pec.fr>
2022-11-22 17:14:38 +0100

    add service-1

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,8 +1,12 @@
 import org.apache.spark.sql.{DataFrame, SparkSession}
+import service1.DeleteClient
 
 object Main {
   def main(args: Array[String]): Unit = {
     val sparkSession = SparkSession.builder().appName("GDPR-COMPLIANCE-APP").master("local").getOrCreate()
+//    val id = args(0).asInstanceOf[Long]
+    val id_test = 1
+    DeleteClient.deleteClient(sparkSession,id_test)
   }
 }
 
