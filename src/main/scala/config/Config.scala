ef7e2aa5c6091d334817bebc333484d8ac787d90
YohanEngineer <bouali.yohan@yahoo.com>
2022-11-22 22:40:27 +0100

    Overwrite with partition

--- src/main/scala/config/Config.scala
+++ src/main/scala/config/Config.scala
@@ -2,5 +2,6 @@ package config
 
 case class Config(
                    delete: Long = 1.toLong,
-                   hash: Long = 1.toLong
+                   hash: Long = 1.toLong,
+                   init:Boolean = false
                  )
