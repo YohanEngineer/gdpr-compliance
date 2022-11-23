2c836033bf45cad1dd9478554a4f791db9ac9a62
YohanEngineer <bouali.yohan@yahoo.com>
2022-11-23 20:46:01 +0100

    Add write and read method for parquet

--- src/main/scala/utils/ParquetTools.scala
+++ src/main/scala/utils/ParquetTools.scala
@@ -1,7 +1,35 @@
 package utils
 
+import config.Client
+import org.apache.spark.sql.{DataFrame, SparkSession}
+
 object ParquetTools {
-  def read(path: String): Unit = {
+  def read(sparkSession: SparkSession, path: String): DataFrame = {
+    import org.apache.spark.sql.Encoders
+
+    val schema = Encoders.product[Client].schema
+
+    val data: DataFrame = sparkSession
+      .read
+      .option("header", true)
+      .option("delimiter", ";")
+      .schema(schema)
+      .parquet(path).coalesce(1)
+
+    data
+  }
+
+  def write(path: String, df: DataFrame): Unit = {
+    df.write
+      .option("header", true)
+      .option("delimiter", ";")
+      .mode("overwrite")
+      .parquet(path)
+
+    import scala.sys.process._
+    s"hdfs dfs -rm -r data/" !
+
+    s"hdfs dfs -mv temp data" !
 
   }
 }
